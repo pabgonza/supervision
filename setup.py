edfc80d9e05516113cadda40df7f716c90c34fe6
Pablo Gonzalez <pablo@gmail.com>
2025-10-17 11:07:50 -0300

    fix: add packages discovery and dependencies to setup.py
    Include find_packages() to ensure all supervision subpackages
    are installed and add all required dependencies when building from git.

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='supervision',
     version='0.27.0rc1',
+    packages=find_packages(include=['supervision*'], exclude=['test*', 'docs*', 'examples*']),
+    install_requires=[
+        "numpy>=1.21.2",
+        "scipy>=1.10.0",
+        "matplotlib>=3.6.0",
+        "pyyaml>=5.3",
+        "defusedxml>=0.7.1",
+        "pillow>=9.4",
+        "requests>=2.26.0",
+        "tqdm>=4.62.3",
+        "opencv-python>=4.5.5.64"
+    ],
 )
